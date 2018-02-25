26e8cc68448d56d44e83762f9349193c19bb5737
ElMoghazy <abdelrhman.almoghazy@student.guc.edu.eg>
2018-02-25 21:42:31 +0200

    solved the deletion problem

--- src/DBAppTest.java
+++ src/DBAppTest.java
@@ -21,48 +21,21 @@ public class DBAppTest {
 
 			ourDB = new DBApp(strTableName);
 
-			Hashtable htblColNameType = new Hashtable();
-			htblColNameType.put("id", "java.lang.Integer");
-			htblColNameType.put("name", "java.lang.String");
-			htblColNameType.put("gpa", "java.lang.double");
-			ourDB.createTable(strTableName, "id", htblColNameType);
-			// createBRINIndex( strTableName, "gpa" );
-			Hashtable htblColNameValue = new Hashtable();
-			htblColNameValue.put("id", new Integer(2343432));
-			htblColNameValue.put("name", new String("Ahmed Noor"));
-			htblColNameValue.put("gpa", new Double(0.95));
-			ourDB.insertIntoTable(strTableName, htblColNameValue);
-			htblColNameValue.clear();
-			htblColNameValue.put("id", new Integer(4253455));
-			htblColNameValue.put("name", new String("Ahmed Ali"));
-			htblColNameValue.put("gpa", new Double(0.95));
-			ourDB.insertIntoTable(strTableName, htblColNameValue);
-			htblColNameValue.clear();
-			htblColNameValue.put("id", new Integer(453455));
-			htblColNameValue.put("name", new String("Dalia Noor"));
-			htblColNameValue.put("gpa", new Double(1.25));
-			ourDB.insertIntoTable(strTableName, htblColNameValue);
-			htblColNameValue.clear();
-			htblColNameValue.put("id", new Integer(23498));
-			htblColNameValue.put("name", new String("John Noor"));
-			htblColNameValue.put("gpa", new Double(1.5));
-			ourDB.insertIntoTable(strTableName, htblColNameValue);
-			htblColNameValue.clear();
-			htblColNameValue.put("id", new Integer(78452));
-			htblColNameValue.put("name", new String("Zaky Noor"));
-			htblColNameValue.put("gpa", new Double(0.88));
-			ourDB.insertIntoTable(strTableName, htblColNameValue);
-			 htblColNameValue.clear();
-			 htblColNameValue.put("id", new Integer(23498));
-			 htblColNameValue.put("name", new String("John Noor"));
-			 htblColNameValue.put("gpa", new Double(1.5));
-			 ourDB.deleteFromTable(strTableName, htblColNameValue);
-			 htblColNameValue.clear();
-			 htblColNameValue.put("id", new Integer(78452));
-			 htblColNameValue.put("name", new String("3ala2 Noor"));
-			 htblColNameValue.put("gpa", new Double(1.08));
-			 ourDB.updateTable(strTableName,"78452", htblColNameValue);
-			 htblColNameValue.clear();
+            ourDB = new DBApp(strTableName);
+
+            Hashtable htblColNameType = new Hashtable();
+            htblColNameType.put("id", "java.lang.Integer");
+            htblColNameType.put("name", "java.lang.String");
+            htblColNameType.put("gpa", "java.lang.double");
+            ourDB.createTable(strTableName, "id", htblColNameType);
+            // createBRINIndex( strTableName, "gpa" );
+            Hashtable htblColNameValue = new Hashtable();
+            htblColNameValue.put("id", new Integer(2343432));
+            htblColNameValue.put("name", new String("Ahmed Noor"));
+            htblColNameValue.put("gpa", new Double(0.95));
+            ourDB.insertIntoTable(strTableName, htblColNameValue);
+            
+            ourDB.deleteFromTable(strTableName, htblColNameValue);
 		} catch (DBAppException D) {
 			System.out.println(D.getMessage());
 		}
