f1ce21ea89645bc2b2a9f6ff9781e432cdec1aa0
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2018-02-25 22:38:06 +0200

    finalized sprint 2

--- src/DBAppException.java
+++ src/DBAppException.java
@@ -4,7 +4,7 @@ public class DBAppException extends Exception {
 	 * Any errors related to the DB App can be detected using this exceptions
 	 */
 	private static final long serialVersionUID = 1L;
-
+int x;
 	public DBAppException(String string) {
 		super(string);
 	}
