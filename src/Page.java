1f0943f6dbf7563f11d764713e85cdf9df596fb8
Amr M. Kayid <amrkayid2027@gmail.com>
2018-03-20 18:16:20 +0200

    Document Page

--- src/Page.java
+++ src/Page.java
@@ -9,126 +9,192 @@ import java.util.Date;
 
 public class Page implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    /**
+     * A page is part of a table holding its tuples.
+     * Tables are stored in many pages in a binary
+     * file format (.class files)
+     */
 
-	private int maximumSize, tupleCount;
-	private String path;
-	private ArrayList<Tuple> tuples;
+    private static final long serialVersionUID = 1L;
 
-	public Page(String path) throws IOException {
-		Configuration config = new Configuration();
-		this.maximumSize = config.getMaximumSize();
-		this.path = path;
-		tuples = new ArrayList<>(maximumSize);
+    private int maximumSize, tupleCount;
+    private String path;
+    private ArrayList<Tuple> tuples;
 
-		// TODO:
-		savePage();
-	}
 
-	// TODO:
-	public boolean insert(Tuple tuple,boolean sort) throws DBAppException, IOException {
+    /**
+     * Create a new page specifying the path at which
+     * the page will be stored relative to the executable files
+     * @param path the path at which the page is stored relative to the executable files
+     * @throws IOException If an I/O error occurred
+     */
+    public Page(String path) throws IOException {
+        Configuration config = new Configuration();
+        this.maximumSize = config.getMaximumSize();
+        this.path = path;
+        tuples = new ArrayList<>(maximumSize);
 
-		if (isFull()) {
-			return false;
-		}
-		tupleCount++;
-		tuples.add(tuple);
-		if(sort)
-		Collections.sort(tuples);
-		savePage();
-		return true;
-	}
-	public boolean delete(Tuple tuple) throws DBAppException, IOException {
-		if(isEmpty())
-			return false;
-		tupleCount--;
-		tuples.remove(tuple);
-		savePage();
-		return true;
+        savePage();
+    }
 
-	}
 
-	public void savePage() throws IOException {
-		File file = new File(path);
-		if (!file.exists())
-			file.createNewFile();
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-		oos.writeObject(this);
-		oos.close();
-	}
+    /**
+     * Save & insert tuples into the page
+     * @param tuple tuple to be inserted
+     * @param sort boolean if the tuples need to be sorted after the insertion
+     * @return true if the tuple was inserted successfully, false otherwise
+     * @throws DBAppException If an DBAppException error occurred
+     * @throws IOException If an I/O error occurred
+     */
+    public boolean insert(Tuple tuple, boolean sort) throws DBAppException, IOException {
 
-	public ArrayList<Tuple> getTuples() {
-		return tuples;
-	}
+        if (isFull()) {
+            return false;
+        }
+        tupleCount++;
+        tuples.add(tuple);
+        if (sort)
+            Collections.sort(tuples);
+        savePage();
+        return true;
+    }
 
-	public void setTuples(ArrayList<Tuple>tuples) {
-		this.tuples = tuples;
-	}
+    /**
+     * delete tuple from the page
+     * @param tuple tuple to be deleted
+     * @return true if the tuple was deleted successfully, false otherwise
+     * @throws DBAppException If an DBAppException error occurred
+     * @throws IOException If an I/O error occurred
+     */
+    public boolean delete(Tuple tuple) throws DBAppException, IOException {
+        if (isEmpty())
+            return false;
+        tupleCount--;
+        tuples.remove(tuple);
+        savePage();
+        return true;
 
-	public int getTupleCount() {
-		return tupleCount;
-	}
+    }
 
-	public boolean isFull() {
-		return tupleCount == maximumSize;
-	}
+    /**
+     * save the page permanently on a secondary storage
+     * @throws IOException If an I/O error occurred
+     */
+    public void savePage() throws IOException {
+        File file = new File(path);
+        if (!file.exists())
+            file.createNewFile();
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+        oos.writeObject(this);
+        oos.close();
+    }
 
-	public void setTupleCount(int tupleCount) {
-		this.tupleCount=tupleCount;
+    /**
+     * check if a certain key exist
+     * @param objKey the search key
+     * @return true if it was found, false otherwise
+     */
+    public boolean exist(Object objKey) {
+        for (Tuple t : tuples) {
+            t.get()[t.getKey()].equals(objKey);
+            switch (t.getTypes()[t.getKey()].toLowerCase()) {
+                case "java.lang.integer":
+                    if (((Integer) t.get()[t.getKey()]).equals(objKey))
+                        return true;
+                case "java.lang.string":
+                    if (((String) t.get()[t.getKey()]).equals(objKey))
+                        return true;
+                case "java.lang.double":
+                    if (((Double) t.get()[t.getKey()]).equals(objKey))
+                        return true;
+                case "java.lang.boolean":
+                    if (((Boolean) t.get()[t.getKey()]).equals(objKey))
+                        return true;
+                case "java.util.date":
+                    if (((Date) t.get()[t.getKey()]).equals(objKey))
+                        return true;
+            }
 
-	}
+        }
+        return false;
+    }
 
-	public boolean isEmpty() {
-		return tupleCount==0;
-	}
+    /**
+     * get a certain tuple according to the search key
+     * @param objKey the search key
+     * @return true if it was found, false otherwise
+     */
+    public Tuple getThisTuple(Object objKey) {
+        for (Tuple t : tuples) {
+            t.get()[t.getKey()].equals(objKey);
+            switch (t.getTypes()[t.getKey()].toLowerCase()) {
+                case "java.lang.integer":
+                    if (((Integer) t.get()[t.getKey()]).equals(objKey))
+                        return t;
+                case "java.lang.string":
+                    if (((String) t.get()[t.getKey()]).equals(objKey))
+                        return t;
+                case "java.lang.double":
+                    if (((Double) t.get()[t.getKey()]).equals(objKey))
+                        return t;
+                case "java.lang.boolean":
+                    if (((Boolean) t.get()[t.getKey()]).equals(objKey))
+                        return t;
+                case "java.util.date":
+                    if (((Date) t.get()[t.getKey()]).equals(objKey))
+                        return t;
+            }
 
-	public boolean exist(Object objKey) {
-		for(Tuple t : tuples){
-			t.get()[t.getKey()].equals(objKey);
-			switch (t.getTypes()[t.getKey()].toLowerCase()) {
-			case "java.lang.integer":
-				if(((Integer)t.get()[t.getKey()]).equals(objKey))
-					return true;
-			case "java.lang.string":
-				if(((String)t.get()[t.getKey()]).equals(objKey))
-					return true;
-			case "java.lang.double":
-				if(((Double)t.get()[t.getKey()]).equals(objKey))
-					return true;
-			case "java.lang.boolean":
-				if(((Boolean)t.get()[t.getKey()]).equals(objKey))
-					return true;
-			case "java.util.date":
-				if(((Date)t.get()[t.getKey()]).equals(objKey))
-					return true;
-			}
+        }
+        return null;
+    }
 
-		}
-		return false;
-	}
+    /**
+     * get tuples arraylist
+     * @return
+     */
+    public ArrayList<Tuple> getTuples() {
+        return tuples;
+    }
 
-	public Tuple getThisTuple(Object objKey) {
-		for(Tuple t : tuples){
-			t.get()[t.getKey()].equals(objKey);
-			switch (t.getTypes()[t.getKey()].toLowerCase()) {
-			case "java.lang.integer":
-				if(((Integer)t.get()[t.getKey()]).equals(objKey))
-					return t;
-			case "java.lang.string":
-				if(((String)t.get()[t.getKey()]).equals(objKey))
-					return t;
-			case "java.lang.double":
-				if(((Double)t.get()[t.getKey()]).equals(objKey))
-					return t;
-			case "java.lang.boolean":
-				if(((Boolean)t.get()[t.getKey()]).equals(objKey))
-					return t;
-			case "java.util.date":
-				if(((Date)t.get()[t.getKey()]).equals(objKey))
-					return t;
-			}
+    /**
+     * set the page's arraylist tuples to new arraylist
+     * @param tuples
+     */
+    public void setTuples(ArrayList<Tuple> tuples) {
+        this.tuples = tuples;
+    }
 
-		}
-		return null;
-	}
+    /**
+     * get number of tuples inside the page
+     * @return
+     */
+    public int getTupleCount() {
+        return tupleCount;
+    }
+
+    /**
+     * check if the page is full
+     * @return
+     */
+    public boolean isFull() {
+        return tupleCount == maximumSize;
+    }
+
+    /**
+     * change the count of tuples inside the page
+     * @param tupleCount
+     */
+    public void setTupleCount(int tupleCount) {
+        this.tupleCount = tupleCount;
+
+    }
+
+    /**
+     * check of the page is empty
+     * @return
+     */
+    public boolean isEmpty() {
+        return tupleCount == 0;
+    }
 }
